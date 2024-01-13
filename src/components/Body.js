ab324a8537e27bd8a5d253078c802804bf2d64e0
Aniket <aniket.ghadge00@gmail.com>
2024-01-13 11:07:49 +0530

    used useState hook for filtering the top resturant

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,18 @@
 import ResturantCard from "./ResturantCard"
 import resList from "../utils/mockData"
+import {useState} from 'react'
 
 const Body = () => {
+    const [restroList, setRestroList] = useState(resList);
     return (
-        <div className='restro_card'>
-            {resList.map((res) => (<ResturantCard key={res.info.id} restData={res}/>))}
+        <div className="body">
+            <button className="top_res_btn" onClick={() => {
+                const filterData = resList.filter((res) => res.info.avgRating > 4.5);
+                setRestroList(filterData);
+            }}>Top Resturants</button>
+            <div className='restro_card'>
+                {restroList.map((res) => (<ResturantCard key={res.info.id} restData={res}/>))}
+            </div>
         </div>
     )
 }
